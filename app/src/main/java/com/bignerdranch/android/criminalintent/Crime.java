d42ee5b7b15c3ae2c197555f562e180bd0025f5f
Chris <chrisabbod@gmail.com>
2018-08-26 21:17:45 -0400

    Add Getters and Setters to Crime Class

--- app/src/main/java/com/bignerdranch/android/criminalintent/Crime.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/Crime.java
@@ -14,4 +14,36 @@ public class Crime {
         mId = UUID.randomUUID();
         mDate = new Date();
     }
+
+    public UUID getId() {
+        return mId;
+    }
+
+    public void setId(UUID mId) {
+        this.mId = mId;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String mTitle) {
+        this.mTitle = mTitle;
+    }
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date mDate) {
+        this.mDate = mDate;
+    }
+
+    public boolean isSolved() {
+        return mSolved;
+    }
+
+    public void setSolved(boolean mSolved) {
+        this.mSolved = mSolved;
+    }
 }
