774cdd8caae7168a6f9f67652a4cc10db7538896
Chris <chrisabbod@gmail.com>
2018-08-30 22:53:15 -0400

    Add ViewPager to CrimePagerActivity and Layout

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimePagerActivity.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimePagerActivity.java
@@ -1,13 +1,45 @@
+//Code Explanation: After finding the ViewPager in the activity's view, you get your data set
+//from CrimeLab - the List of crimes. Next, you get the activity's instance of FragmentManager.
+//Then you set the adapter to be an unnamed instance of FragmentStatePagerAdapter.
+//The adapter is adding the fragments you return to your activity and helping ViewPager identify
+//the fragments' views so that they can be placed correctly.
+
 package com.bignerdranch.android.criminalintent;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 
+import java.util.List;
+
 public class CrimePagerActivity extends AppCompatActivity {
+
+    private ViewPager mViewPager;
+    private List<Crime> mCrimes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crime_pager);
+
+        mViewPager = findViewById(R.id.crime_view_pager);
+
+        mCrimes = CrimeLab.get(this).getCrimes();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        mViewPager.setAdapter(new FragmentPagerAdapter(fragmentManager) {
+            @Override
+            public Fragment getItem(int position) {
+                Crime crime = mCrimes.get(position);
+                return CrimeFragment.newInstance(crime.getId());
+            }
+
+            @Override
+            public int getCount() {
+                return mCrimes.size();
+            }
+        });
     }
 }
