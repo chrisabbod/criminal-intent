4f2b1956ef24cdacb08c112d2b0704d94b65b2a8
Chris <chrisabbod@gmail.com>
2018-09-02 21:18:17 -0400

    Add Columns to CrimeBaseHelper onCreate() Function

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeLab.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeLab.java
@@ -1,6 +1,9 @@
 package com.bignerdranch.android.criminalintent;
 
 import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.bignerdranch.android.criminalintent.database.CrimeBaseHelper;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -10,6 +13,8 @@ public class CrimeLab {
     private static CrimeLab sCrimeLab;
 
     private List<Crime> mCrimes;
+    private Context mContext;
+    private SQLiteDatabase mDatabase;
 
     public static CrimeLab get(Context context){
         if(sCrimeLab == null){
@@ -18,7 +23,16 @@ public class CrimeLab {
         return sCrimeLab;
     }
 
+    //When you call getWritableDatabase() here, CrimeBaseHelper will do the following
+    //Open up /data/data/com.bignerdranch.android.criminalintent/databases/crimeBase.db
+    //creating a new database file if it does not already exist
+    //If this is the first time the database has been created, call onCreate(SQLiteDatabase), then save
+    //out the latest version number
+    //If this is not the first time, check the version number in the database. If the version number in
+    //CrimeBaseHelper is higher, call onUpgrade(SQLiteDatabase, int, int)
     private CrimeLab(Context context){
+        mContext = context.getApplicationContext();
+        mDatabase = new CrimeBaseHelper(mContext).getWritableDatabase();
         mCrimes = new ArrayList<>();
     }
 
--- app/src/main/java/com/bignerdranch/android/criminalintent/database/CrimeDbSchema.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/database/CrimeDbSchema.java
@@ -4,14 +4,15 @@ public class CrimeDbSchema {
     //CrimeTable only exists to define the String constants needed to describe the moving pieces of your table definition
     public static final class CrimeTable {
         public static final String NAME = "crimes";
-    }
 
-    //Allows you to refer to columns in a safe way
-    //CrimeTable.Cols.TITLE is safer to update if you ever need to change the name of that column
-    public static final class Cols{
-        public static final String UUID = "uuid";
-        public static final String TITLE = "title";
-        public static final String Date = "date";
-        public static final String SOLVED = "solved";
+
+        //Allows you to refer to columns in a safe way
+        //CrimeTable.Cols.TITLE is safer to update if you ever need to change the name of that column
+        public static final class Cols {
+            public static final String UUID = "uuid";
+            public static final String TITLE = "title";
+            public static final String Date = "date";
+            public static final String SOLVED = "solved";
+        }
     }
 }
