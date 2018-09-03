2dca7ec934f8267352aef61eae469add0e917119
Chris <chrisabbod@gmail.com>
2018-09-02 20:53:49 -0400

    Create CrimeDbSchema Class

---
+++ app/src/main/java/com/bignerdranch/android/criminalintent/database/CrimeDbSchema.java
@@ -0,0 +1,17 @@
+package com.bignerdranch.android.criminalintent.database;
+
+public class CrimeDbSchema {
+    //CrimeTable only exists to define the String constants needed to describe the moving pieces of your table definition
+    public static final class CrimeTable {
+        public static final String NAME = "crimes";
+    }
+
+    //Allows you to refer to columns in a safe way
+    //CrimeTable.Cols.TITLE is safer to update if you ever need to change the name of that column
+    public static final class Cols{
+        public static final String UUID = "uuid";
+        public static final String TITLE = "title";
+        public static final String Date = "date";
+        public static final String SOLVED = "solved";
+    }
+}
